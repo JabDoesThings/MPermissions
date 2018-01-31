7c60e7911b2ecd508aac7625a9e7806cdb2b64a7
Jab <40BlocksUnder@gmail.com>
2018-01-31 17:26:57 -0600

    Initial commit.

---
+++ src/main/java/jab/bukkit/mpermissions/PluginMPermissions.java
@@ -0,0 +1,23 @@
+package jab.bukkit.mpermissions;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMPermissions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        return false;
+    }
+}
